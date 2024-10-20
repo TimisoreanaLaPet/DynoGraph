04322bdc0740e8330cf5fa7e3eb5d558f353f44c
Cristina <c.neacsu@lancaster.ac.uk>
2024-10-20 19:20:32 +0200

    #1 - Create Theme

--- DynoGraph/src/App.tsx
+++ DynoGraph/src/App.tsx
@@ -1,7 +1,18 @@
+import {createTheme, ThemeProvider} from "@mui/material";
+import {useMemo} from "react";
+import {themeSettings} from "./theme/theme.ts";
+import TopBar from "./components/TopBar.tsx";
+
 function App() {
+  const mode = 'dark'
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
+
   return (
     <div className="app">
-      hello world
+      <ThemeProvider theme={theme}>
+        hello world
+        <TopBar/>
+      </ThemeProvider>
     </div>
   )
 }
