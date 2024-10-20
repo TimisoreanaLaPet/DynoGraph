a7fe3d602f80cda3580f73e89c5045b5d8453ba1
Cristina <c.neacsu@lancaster.ac.uk>
2024-10-20 19:21:13 +0200

    #1 FE - new component: TopBar.tsx

---
+++ DynoGraph/src/components/TopBar.tsx
@@ -0,0 +1,15 @@
+import {AppBar, Button, Toolbar} from "@mui/material";
+
+function TopBar() {
+  return (
+    <AppBar>
+      <Toolbar>
+        <Button variant={"text"}>
+          test
+        </Button>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default TopBar;
