92ef27954fc64a9e15f57b1906f6196b8e86de69
Cristina <c.neacsu@lancaster.ac.uk>
2024-10-27 18:16:17 +0100

    #3 Backend - Initial Commit

---
+++ dyno-graph-be/src/test/java/com/dynograph/DynoGraphApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dynograph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynoGraphApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
